d70501eecb81f213d26c65a3181ed554c13c5ba8
dudtla <163372789+dudtla@users.noreply.github.com>
2024-03-19 10:02:00 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
-           answer = (int)(((double)num1 / num2) * 1000);
+           answer = (int)(((double)num1 / num2) * 1000); //num1,num2둘중에 하나에만 형변환 해주어야 함 
          return answer;
     }
 }
