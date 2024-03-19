f89aebebb792cbb08d938ed8673ca66cc78eaa52
dudtla <163372789+dudtla@users.noreply.github.com>
2024-03-19 09:26:59 +0900

    Update 짝수의 합.java

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i = 1; i <= n; i++ ) {
-			if(i % 2 == 0) {
+        for(int i = 1; i <= n; i++ ) { //i부터 n까지 1씩 더해가며 반복한다
+			if(i % 2 == 0) { //i를 2로 나누었을 때 나누어 떨어지면 i를 더해준다. 
 				answer += i;
 			}
 			
